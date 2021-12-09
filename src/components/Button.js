3581240f61a95bc265f54c65fe9c7ce93b5cffeb
RAJINIA <rajini1608@gmail.com>
2021-12-08 21:04:18 -0600

    Add Building Button and DayListItem

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 import "components/Button.scss";
+import className from "classnames";
+
 
 export default function Button(props) {
-   return <></>;
+   className('button', 'button--confirm');
+   className('button', 'button--danger');
+
+   let buttonClass = className("button",{
+      "button--confirm": props.confirm,
+      "button--danger":props.danger
+   } );
+   
+   
+   return (
+      <button 
+         className={buttonClass}
+         onClick={props.onClick}
+         disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+   )
 }
+
